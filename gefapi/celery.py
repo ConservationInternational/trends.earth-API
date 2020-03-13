35feaffca7bc2b5233e9e4cbcb39c1a50f5c00f3
Alex Zvoleff <azvoleff@gmail.com>
2020-03-13 13:29:29 -0400

    Add celery support for rollbar.

--- gefapi/celery.py
+++ gefapi/celery.py
@@ -1,4 +1,22 @@
+import os
+
 from celery import Celery
+from celery.signals import task_failure
+
+import rollbar
+
+rollbar.init(os.getenv('ROLLBAR_SERVER_TOKEN'), os.getenv('ENV'))
+
+
+def celery_base_data_hook(request, data):
+    data['framework'] = 'celery'
+    rollbar.BASE_DATA_HOOK = celery_base_data_hook
+
+
+@task_failure.connect
+def handle_task_failure(**kw):
+    rollbar.report_exc_info(extra_data=kw)
+
 
 def make_celery(app):
     celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
