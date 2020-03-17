4e0a4a8d6d05fe735d6015574aa22dd898a6199e
Alex Zvoleff <azvoleff@gmail.com>
2020-03-17 08:12:11 -0400

    Fix import order.

--- gefapi/services/__init__.py
+++ gefapi/services/__init__.py
@@ -1,12 +1,12 @@
 """GEFAPI SERVICES MODULE"""
 
-import os
-import sys
-import logging
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
+import os
+import sys
+import logging
 
 import rollbar
 from rollbar.logger import RollbarHandler
