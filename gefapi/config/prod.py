300b41d3c6717f61693c74bcf2fda51d4f3d99bf
Alex Zvoleff <azvoleff@conservation.org>
2021-11-16 13:28:48 +0000

    Fix prod env vars

--- gefapi/config/prod.py
+++ gefapi/config/prod.py
@@ -7,14 +7,5 @@ SETTINGS = {
     'service': {
         'port': 3000
     },
-    'environment': {
-        'ROLLBAR_SCRIPT_TOKEN': os.getenv('ROLLBAR_SCRIPT_TOKEN'),
-        'ROLLBAR_SERVER_TOKEN': os.getenv('ROLLBAR_SERVER_TOKEN'),
-        'EE_PRIVATE_KEY': os.getenv('EE_PRIVATE_KEY'),
-        'EE_SERVICE_ACCOUNT': os.getenv('EE_SERVICE_ACCOUNT'),
-        'EE_SERVICE_ACCOUNT_JSON': os.getenv('EE_SERVICE_ACCOUNT_JSON')
-    },
-    'SCRIPTS_S3_PREFIX': os.getenv('SCRIPTS_S3_PREFIX'),
-    'SCRIPTS_S3_BUCKET': os.getenv('SCRIPTS_S3_BUCKET'),
     'REGISTRY_URL': os.getenv('REGISTRY_URL'),
 }
