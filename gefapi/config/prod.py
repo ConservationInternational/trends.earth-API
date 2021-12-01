ac9d73a5093e517821b3e186a360ca3bf96d3c41
Alex Zvoleff <azvoleff@conservation.org>
2021-12-01 08:30:39 -0500

    Whitespace

--- gefapi/config/prod.py
+++ gefapi/config/prod.py
@@ -1,9 +1,8 @@
 import os
 import urllib.request
 
-
-instance_ip = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/local-ipv4').read().decode()
-
+instance_ip = urllib.request.urlopen(
+    'http://169.254.169.254/latest/meta-data/local-ipv4').read().decode()
 
 SETTINGS = {
     'logging': {
@@ -12,7 +11,10 @@ SETTINGS = {
     'service': {
         'port': 3000
     },
-    'REGISTRY_URL': os.getenv('REGISTRY_URL'),
-    'CELERY_BROKER_URL': 'redis://'+instance_ip+':' + os.getenv('REDIS_PORT_6379_TCP_PORT'),
-    'CELERY_RESULT_BACKEND':'redis://'+instance_ip+':' + os.getenv('REDIS_PORT_6379_TCP_PORT')
+    'REGISTRY_URL':
+    os.getenv('REGISTRY_URL'),
+    'CELERY_BROKER_URL':
+    'redis://' + instance_ip + ':' + os.getenv('REDIS_PORT_6379_TCP_PORT'),
+    'CELERY_RESULT_BACKEND':
+    'redis://' + instance_ip + ':' + os.getenv('REDIS_PORT_6379_TCP_PORT')
 }
