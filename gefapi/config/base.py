ecae38c52d73fde1ad4be07626b01b2f81d9110f
Alex Zvoleff <azvoleff@conservation.org>
2021-12-01 08:30:56 -0500

    Add endpoint and project ID for new highvolume GEE endpoint

--- gefapi/config/base.py
+++ gefapi/config/base.py
@@ -1,4 +1,3 @@
-
 import os
 from datetime import timedelta
 
@@ -18,13 +17,15 @@ SETTINGS = {
         'SPARKPOST_API_KEY': os.getenv('SPARKPOST_API_KEY'),
         'API_URL': os.getenv('API_URL'),
         'API_USER': os.getenv('API_USER'),
-        'API_PASSWORD': os.getenv('API_PASSWORD')
+        'API_PASSWORD': os.getenv('API_PASSWORD'),
+        'GEE_ENDPOINT': 'https://earthengine-highvolume.googleapis.com',
+        'GOOGLE_PROJECT': '1080184168142'
     },
     'ROLES': ['ADMIN', 'USER'],
-    'SQLALCHEMY_DATABASE_URI': 'postgresql://'+os.getenv('DATABASE_ENV_POSTGRES_USER')+':'+os.getenv('DATABASE_ENV_POSTGRES_PASSWORD')+'@'+os.getenv('DATABASE_PORT_5432_TCP_ADDR')+':'+os.getenv('DATABASE_PORT_5432_TCP_PORT')+'/'+os.getenv('DATABASE_ENV_POSTGRES_DB'),
+    'SQLALCHEMY_DATABASE_URI': 'postgresql://' + os.getenv('DATABASE_ENV_POSTGRES_USER') + ':' + os.getenv('DATABASE_ENV_POSTGRES_PASSWORD') + '@' + os.getenv('DATABASE_PORT_5432_TCP_ADDR') + ':' + os.getenv('DATABASE_PORT_5432_TCP_PORT') + '/' + os.getenv('DATABASE_ENV_POSTGRES_DB'),
     'SECRET_KEY': 'mysecret',
     'DOCKER_URL': os.getenv('DOCKER_URL'),
-    'REGISTRY_URL': 'localhost:'+os.getenv('REGISTRY_PORT_5000_TCP_PORT', ''),
+    'REGISTRY_URL': 'localhost:' + os.getenv('REGISTRY_PORT_5000_TCP_PORT', ''),
     'SCRIPTS_S3_PREFIX': os.getenv('SCRIPTS_S3_PREFIX'),
     'SCRIPTS_S3_BUCKET': os.getenv('SCRIPTS_S3_BUCKET'),
     'UPLOAD_FOLDER': '/tmp/scripts',
@@ -32,6 +33,6 @@ SETTINGS = {
     'JWT_AUTH_USERNAME_KEY': 'email',
     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
     'JWT_EXPIRATION_DELTA': timedelta(seconds=60*60*24),
-    'CELERY_BROKER_URL': 'redis://'+os.getenv('REDIS_PORT_6379_TCP_ADDR')+':' + os.getenv('REDIS_PORT_6379_TCP_PORT'),
-    'CELERY_RESULT_BACKEND': 'redis://'+os.getenv('REDIS_PORT_6379_TCP_ADDR')+':' + os.getenv('REDIS_PORT_6379_TCP_PORT')
+    'CELERY_BROKER_URL': 'redis://' + os.getenv('REDIS_PORT_6379_TCP_ADDR') + ':' + os.getenv('REDIS_PORT_6379_TCP_PORT'),
+    'CELERY_RESULT_BACKEND': 'redis://' + os.getenv('REDIS_PORT_6379_TCP_ADDR') + ':' + os.getenv('REDIS_PORT_6379_TCP_PORT')
 }
