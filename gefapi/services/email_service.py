65c3b0cc81001f49436fc238fd1eb5cc3a86f6ed
Alex Zvoleff <azvoleff@conservation.org>
2022-01-05 08:20:11 -0500

    Don't fail on sparkpost error

--- gefapi/services/email_service.py
+++ gefapi/services/email_service.py
@@ -6,25 +6,24 @@ from __future__ import print_function
 
 import logging
 
-from gefapi.errors import EmailError
 from sparkpost import SparkPost
 
 
 class EmailService(object):
     """MailService Class"""
-
     @staticmethod
-    def send_html_email(recipients=[], html='', from_email='api@trends.earth', subject='[trends.earth] Undefined Subject'):
+    def send_html_email(recipients=[],
+                        html='',
+                        from_email='api@trends.earth',
+                        subject='[trends.earth] Undefined Subject'):
+        logging.debug('Sending email with subject %s' % (subject))
         try:
             sp = SparkPost()
-            response = sp.transmissions.send(
-                recipients=recipients,
-                html=html,
-                from_email=from_email,
-                subject=subject
-            )
+            response = sp.transmissions.send(recipients=recipients,
+                                             html=html,
+                                             from_email=from_email,
+                                             subject=subject)
 
             return response
         except Exception as error:
-            logging.error(error)
-            raise EmailError(message=error)
+            logging.exception(error)
