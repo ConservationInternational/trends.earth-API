08c6d1795ec2cb9429c904ac2b172425d82c3ec1
Alex Zvoleff <azvoleff@conservation.org>
2025-07-07 19:59:14 -0400

    Don't set S3 default prefix/bucket

--- gefapi/s3.py
+++ gefapi/s3.py
@@ -10,13 +10,19 @@ logger = logging.getLogger()
 
 
 def push_script_to_s3(file_path, object_basename):
-    object_name = SETTINGS.get("SCRIPTS_S3_PREFIX") + "/" + object_basename
+    prefix = SETTINGS.get("SCRIPTS_S3_PREFIX")
+    bucket = SETTINGS.get("SCRIPTS_S3_BUCKET")
+
+    if not prefix:
+        raise ValueError("SCRIPTS_S3_PREFIX configuration is required")
+    if not bucket:
+        raise ValueError("SCRIPTS_S3_BUCKET configuration is required")
+
+    object_name = prefix + "/" + object_basename
     logger.info("[SERVICE]: Saving %s to S3", object_name)
     s3_client = boto3.client("s3")
     try:
-        _ = s3_client.upload_file(
-            str(file_path), SETTINGS.get("SCRIPTS_S3_BUCKET"), object_name
-        )
+        _ = s3_client.upload_file(str(file_path), bucket, object_name)
     except botocore.exceptions.ClientError as e:
         logger.error(e)
         rollbar.report_exc_info()
@@ -25,19 +31,54 @@ def push_script_to_s3(file_path, object_basename):
 
 
 def get_script_from_s3(script_file, out_path):
-    object_name = SETTINGS.get("SCRIPTS_S3_PREFIX") + "/" + script_file
+    prefix = SETTINGS.get("SCRIPTS_S3_PREFIX")
+    bucket = SETTINGS.get("SCRIPTS_S3_BUCKET")
+
+    if not prefix:
+        raise ValueError("SCRIPTS_S3_PREFIX configuration is required")
+    if not bucket:
+        raise ValueError("SCRIPTS_S3_BUCKET configuration is required")
+
+    object_name = prefix + "/" + script_file
     s3 = boto3.client("s3")
-    s3.download_file(SETTINGS.get("SCRIPTS_S3_BUCKET"), object_name, out_path)
+    s3.download_file(bucket, object_name, out_path)
+
+
+def delete_script_from_s3(object_basename):
+    prefix = SETTINGS.get("SCRIPTS_S3_PREFIX")
+    bucket = SETTINGS.get("SCRIPTS_S3_BUCKET")
+
+    if not prefix:
+        raise ValueError("SCRIPTS_S3_PREFIX configuration is required")
+    if not bucket:
+        raise ValueError("SCRIPTS_S3_BUCKET configuration is required")
+
+    object_name = prefix + "/" + object_basename
+    logger.info("[SERVICE]: Deleting %s from S3", object_name)
+    s3_client = boto3.client("s3")
+    try:
+        s3_client.delete_object(Bucket=bucket, Key=object_name)
+    except botocore.exceptions.ClientError as e:
+        logger.error(e)
+        rollbar.report_exc_info()
+        return False
+    return True
 
 
 def push_params_to_s3(file_path, object_basename):
-    object_name = SETTINGS.get("PARAMS_S3_PREFIX") + "/" + object_basename
+    prefix = SETTINGS.get("PARAMS_S3_PREFIX")
+    bucket = SETTINGS.get("PARAMS_S3_BUCKET")
+
+    if not prefix:
+        raise ValueError("PARAMS_S3_PREFIX configuration is required")
+    if not bucket:
+        raise ValueError("PARAMS_S3_BUCKET configuration is required")
+
+    object_name = prefix + "/" + object_basename
     logger.info("[SERVICE]: Saving %s to S3", object_name)
     s3_client = boto3.client("s3")
     try:
-        _ = s3_client.upload_file(
-            str(file_path), SETTINGS.get("PARAMS_S3_BUCKET"), object_name
-        )
+        _ = s3_client.upload_file(str(file_path), bucket, object_name)
     except botocore.exceptions.ClientError as e:
         logger.error(e)
         rollbar.report_exc_info()
