d489ad06c1124994559f46c5681c52b687edf344
Alex Zvoleff <azvoleff@conservation.org>
2026-05-04 09:23:55 -0400

    Fix migration

--- migrations/versions/d49638469453_add_bulk_email_fields_data.py
+++ migrations/versions/d49638469453_add_bulk_email_fields_data.py
@@ -1,15 +1,15 @@
 """Add fields_data column to bulk_email table.
 
-Revision ID: c1d2e3f4a5b6
+Revision ID: d49638469453
 Revises: b5c6d7e8f9a0
-Create Date: 2025-01-01 00:00:00.000000
+Create Date: 2025-05-04 00:00:00.000000
 """
 
 import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = "c1d2e3f4a5b6"
+revision = "d49638469453"
 down_revision = "b5c6d7e8f9a0"
 branch_labels = None
 depends_on = None
